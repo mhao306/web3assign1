a2f29a0a50d8bd0eaba08d632fa862a35c5b7840
Michelle Hao <mhao306@mtroyal.ca>
2025-10-20 22:15:22 -0600

    finished routes, added error handling, added env vars

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# COMP 4513 Assignment 1
+## Overview
+This repository contains a Node API for querying a Formula 1 car racing database. This includes tables for circuits, constructors, drivers, races, and results. The data is return in JSON format.
+
+## Example API Requests for Testing
+
 http://localhost:8080/api/circuits
 http://localhost:8080/api/circuits/monza
 <!-- do error handling -->
@@ -16,3 +22,7 @@ http://localhost:8080/api/races/circuits/spa
 http://localhost:8080/api/races/circuits/monza/season/2018/2020
 http://localhost:8080/api/results/11
 http://localhost:8080/api/results/driver/sato
+http://localhost:8080/api/results/drivers/speed/seasons/2007/2008
+http://localhost:8080/api/qualifying/15
+http://localhost:8080/api/standings/drivers/15
+http://localhost:8080/api/standings/constructors/15
--- router.js
+++ router.js
@@ -2,183 +2,445 @@ const express = require('express');
 const supa = require('@supabase/supabase-js');
 const app = express();
 
-const supaUrl = 'https://bfkxfsozvqrijtkcfsgd.supabase.co';
-const supaAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJma3hmc296dnFyaWp0a2Nmc2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NzU1NjEsImV4cCI6MjA3NTQ1MTU2MX0.ILdJsta-8ppFd-Ji3fR5ZSjSTQkDiPQiKtpIjnhBAoc';
-
-const supabase = supa.createClient(supaUrl, supaAnonKey);
+const supabase = supa.createClient(process.env.supaUrl, process.env.supaAnonKey);
 
 app.get('/api/circuits', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('circuits')
-        .select()
-    if (err) {
-        console.error('Error fetching data:', err);
-        return
+    try {
+        const { data, error } = await supabase
+            .from('circuits')
+            .select()
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found." })
+        }
+
+        resp.send(data);
+    } catch (err) {
+        console.error("Error fetching circuit results:", err.message);
     }
-    resp.send(data);
 })
 
 app.get('/api/circuits/:ref', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('circuits')
-        .select()
-        .eq('circuitRef', req.params.ref)
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('circuits')
+            .select()
+            .eq('circuitRef', req.params.ref)
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for circuitRef: " + req.params.ref })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching circuit results:", err.message);
+    }
 })
 
 app.get('/api/circuits/season/:year', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('races')
-        .select('round, circuits (*), year')
-        .eq('year', req.params.year)
-        .order('round', {ascending: true});
-    resp.send(data);
+    try {
+        const { data, error } = await supabase
+            .from('races')
+            .select('round, circuits (*), year')
+            .eq('year', req.params.year)
+            .order('round', { ascending: true });
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for year: " + req.params.year })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching circuit results:", err.message);
+    }
 })
 
 app.get('/api/constructors', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('constructors')
-        .select()
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('constructors')
+            .select()
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for constructors" })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching constructor results:", err.message);
+    }
 })
 
 app.get('/api/constructors/:ref', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('constructors')
-        .select()
-        .eq('constructorRef', req.params.ref)
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('constructors')
+            .select()
+            .eq('constructorRef', req.params.ref)
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for constructorRef: " + req.params.ref })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching constructor results:", err.message);
+    }
 })
 
 app.get('/api/drivers', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('drivers')
-        .select()
-    resp.send(data)
+    try {
+
+        const { data, error } = await supabase
+            .from('drivers')
+            .select()
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for drivers" })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching driver results:", err.message);
+    }
 })
 
 app.get('/api/drivers/:ref', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('drivers')
-        .select()
-        .eq('driverRef', req.params.ref)
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('drivers')
+            .select()
+            .eq('driverRef', req.params.ref)
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for driverRef: " + req.params.ref })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching driver results:", err.message);
+    }
 })
 
 app.get('/api/drivers/search/:substring', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('drivers')
-        .select()
-        .ilike('surname', `${req.params.substring}%`)
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('drivers')
+            .select()
+            .ilike('surname', `${req.params.substring}%`)
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for surname substring: " + req.params.substring })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching driver results:", err.message);
+    }
 })
 
 app.get('/api/drivers/race/:raceId', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('results')
-        .select('raceId, drivers (*)')
-        .eq('raceId', req.params.raceId);
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('results')
+            .select('raceId, drivers (*)')
+            .eq('raceId', req.params.raceId);
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            // error = {Error: "No results found for raceId: " + req.params.raceId }
+            // resp.status(404)
+            return resp.status(404).json({ error: "No results found for raceId: " + req.params.raceId })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching driver results:", err.message);
+    }
 })
 
 app.get('/api/races/:raceId', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('races')
-        .select('raceId, year, round, name, date, time, url, circuits (name, location, country)')
-        .eq('raceId', req.params.raceId)
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('races')
+            .select('raceId, year, round, name, date, time, url, circuits (name, location, country)')
+            .eq('raceId', req.params.raceId)
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for raceId: " + req.params.raceId })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching race results:", err.message);
+    }
 })
 
 app.get('/api/races/season/:year', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('races')
-        .select()
-        .eq('year', req.params.year)
-        .order('round', {ascending: true})
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('races')
+            .select()
+            .eq('year', req.params.year)
+            .order('round', { ascending: true })
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for year: " + req.params.year })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching race results:", err.message);
+    }
 })
 
 app.get('/api/races/season/:year/:round', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('races')
-        .select()
-        .eq('year', req.params.year)
-        .order('round', {ascending: true})
-        .eq('round', req.params.round)
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('races')
+            .select()
+            .eq('year', req.params.year)
+            .order('round', { ascending: true })
+            .eq('round', req.params.round)
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for year (" + req.params.year } + ") and round (" + req.params.round)
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching race results:", err.message);
+    }
 })
 
 app.get('/api/races/circuits/:ref', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('races')
-        .select('raceId, year, circuits!inner (circuitRef, name)')
-        .eq('circuits.circuitRef', req.params.ref)
-        .order('year', {ascending: true})
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('races')
+            .select('raceId, year, circuits!inner (circuitRef, name)')
+            .eq('circuits.circuitRef', req.params.ref)
+            .order('year', { ascending: true })
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for circuitRef: " + req.params.ref })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching race results:", err.message);
+    }
 })
 
 app.get('/api/races/circuits/:ref/season/:start/:end', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('races')
-        .select('raceId, year, date, circuits!inner (circuitId, circuitRef, name)')
-        .eq('circuits.circuitRef', req.params.ref)
-        .gte('year', req.params.start)
-        .lte('year', req.params.end)
-        .order('date', {ascending: true})
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('races')
+            .select('raceId, year, date, circuits!inner (circuitId, circuitRef, name)')
+            .eq('circuits.circuitRef', req.params.ref)
+            .gte('year', req.params.start)
+            .lte('year', req.params.end)
+            .order('date', { ascending: true })
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (req.params.start > req.params.end) {
+            return resp.status(404).json({ error: "Start year is later than end year" })
+        }
+        if (req.params.end < req.params.start) {
+            return resp.status(404).json({ error: "End year is earlier than start year" })
+        }
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for circuitRef(" + req.params.ref + "), start (" + req.params.start + "), and end (" + req.params.end + ")" })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching race results:", err.message);
+    }
 })
 
 app.get('/api/results/:raceId', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('results')
-        .select('grid, drivers (driverRef, code, forename, surname), races (name, round, year, date), constructors (name, constructorRef, nationality)')
-        .eq('raceId', req.params.raceId)
-        .order('grid', {ascending: true})
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('results')
+            .select('grid, drivers (driverRef, code, forename, surname), races (name, round, year, date), constructors (name, constructorRef, nationality)')
+            .eq('raceId', req.params.raceId)
+            .order('grid', { ascending: true })
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for raceId: " + req.params.raceId })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching result results:", err.message);
+    }
 })
 
 app.get('/api/results/driver/:ref', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('results')
-        .select('resultId, raceId, positionOrder, drivers!inner (driverRef, forename, surname)')
-        .eq('drivers.driverRef', req.params.ref)
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('results')
+            .select('resultId, raceId, positionOrder, drivers!inner (driverRef, forename, surname)')
+            .eq('drivers.driverRef', req.params.ref)
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for driverRef: " + req.params.ref })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching result results:", err.message);
+    }
 })
 
-// left off here
+// need to fix ordering
 app.get('/api/results/drivers/:ref/seasons/:start/:end', async (req, resp) => {
-    const {data, err} = await supabase
-        .from('results')
-        .select('resultId, raceId, races!inner (year, date), positionOrder, drivers (driverRef, forename, surname)')
-        .eq('drivers.driverRef', req.params.ref)
-        .gte('races.year', req.params.start)
-        .lte('races.year', req.params.end)
-        .order('races.date', {referencedTable: 'results', ascending:true})
-    resp.send(data)
+    try {
+        const { data, error } = await supabase
+            .from('results')
+            .select('resultId, raceId, positionOrder, drivers!inner (driverRef, forename, surname), races!inner (raceId, year, date)')
+            .eq('drivers.driverRef', req.params.ref)
+            .gte('races.year', req.params.start)
+            .lte('races.year', req.params.end)
+            .order('year', { referencedTable: 'races', ascending: true })
+        // .order('date', { referencedTable: 'races', ascending: true});
+
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (req.params.start > req.params.end) {
+            return resp.status(404).json({ error: "Start year is later than end year" })
+        }
+        if (req.params.end < req.params.start) {
+            return resp.status(404).json({ error: "End year is earlier than start year" })
+        }
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for driverRef(" + req.params.ref + "), start (" + req.params.start + "), and end (" + req.params.end + ")" })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching result results:", err.message);
+    }
 })
 
-app.get('', async (req, resp) => {
-    const {data, err} = await supabase
-        .from()
-        .select()
+app.get('/api/qualifying/:raceId', async (req, resp) => {
+    try {
+        const { data, error } = await supabase
+            .from('qualifying')
+            .select('qualifyId, number, position, races!inner (raceId, year, round, name, date), drivers!inner (driverRef, forename, surname), constructors!inner (constructorRef, name)')
+            .eq('races.raceId', req.params.raceId)
+            .order('position', { ascending: true })
 
-    resp.send(data)
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for raceId: " + req.params.raceId })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching qualifying results:", err.message);
+    }
 })
 
-app.get('', async (req, resp) => {
-    const {data, err} = await supabase
-        .from()
-        .select()
+app.get('/api/standings/drivers/:raceId', async (req, resp) => {
+    try {
+        const { data, error } = await supabase
+            .from('driver_standings')
+            .select('raceId, drivers!inner (forename, surname), points, position, wins, races!inner (year, name)')
+            .eq('races.raceId', req.params.raceId)
+            .order('position', { ascending: true })
 
-    resp.send(data)
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for raceId: " + req.params.raceId })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching driver standing results:", err.message);
+    }
 })
 
-app.get('', async (req, resp) => {
-    const {data, err} = await supabase
-        .from()
-        .select()
+app.get('/api/standings/constructors/:raceId', async (req, resp) => {
+    try {
+        const { data, error } = await supabase
+            .from('constructor_standings')
+            .select('raceId, points, position, constructors!inner (constructorRef, name), races!inner (year, name)')
+            .eq('races.raceId', req.params.raceId)
+            .order('position', { ascending: true })
 
-    resp.send(data)
+        if (error) {
+            console.error("Supabase error:", error);
+            return resp.status(400).json({ error: error.message });
+        }
+
+        if (!data || data.length == 0) {
+            return resp.status(404).json({ error: "No results found for raceId: " + req.params.raceId })
+        }
+
+        resp.send(data)
+    } catch (err) {
+        console.error("Error fetching constructor standing results:", err.message);
+    }
 })
 
 app.listen(8080, () => {
